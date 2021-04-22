ef5a9753d848faa1d87e91ec55318b4e24ea2853
SemiShogun <nectoj@gmail.com>
2021-04-23 01:23:44 +0200

    Update Copyright

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -21,23 +21,27 @@ function Footer() {
           </Modal.Header>
           <Modal.Body>
             <p>
-              MIT License Copyright (c) 2021 Jamie Lam Permission is hereby
-              granted, free of charge, to any person obtaining a copy of this
-              software and associated documentation files (the "Software"), to
-              deal in the Software without restriction, including without
-              limitation the rights to use, copy, modify, merge, publish,
-              distribute, sublicense, and/or sell copies of the Software, and to
-              permit persons to whom the Software is furnished to do so, subject
-              to the following conditions: The above copyright notice and this
-              permission notice shall be included in all copies or substantial
-              portions of the Software. THE SOFTWARE IS PROVIDED "AS IS",
-              WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-              NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-              PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-              OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-              OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-              OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+              MIT License Copyright (c)
+              <br />
+              2021 Jamie Lam Permission is hereby granted, free of charge, to
+              any person obtaining a copy of this software and associated
+              documentation files (the "Software"), to deal in the Software
+              without restriction, including without limitation the rights to
+              use, copy, modify, merge, publish, distribute, sublicense, and/or
+              sell copies of the Software, and to permit persons to whom the
+              Software is furnished to do so, subject to the following
+              conditions: The above copyright notice and this permission notice
+              shall be included in all copies or substantial portions of the
+              Software.
+              <br />
+              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+              EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+              MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+              NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+              HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+              WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+              DEALINGS IN THE SOFTWARE.
             </p>
             <br />
             <div>
@@ -53,13 +57,20 @@ function Footer() {
                 www.flaticon.com
               </a>
             </div>
+            <br />
+            <p>
+              Footer Icons made by Ismael Martínez from{" "}
+              <a
+                href="https://github.com/ismamz/react-bootstrap-icons"
+                title="react-bootstrap-icons"
+              >
+                react-bootstrap-icons
+              </a>
+            </p>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
             </Button>
           </Modal.Footer>
         </Modal>
